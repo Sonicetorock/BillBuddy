8d3c92be1ec8a75719306b4f940aa0866b98acd7
Sonicetorock <sonishgames@gmail.com>
2024-10-23 19:46:30 +0530

    Updates both routes

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { addExpense, getUserExpenses, getOverallExpenses } = require('../controllers/expenseController');
+const { validatePercentageSplit } = require('../middlewares/validateSplit');
 
 const router = express.Router();
+
+router.post('/expenses', validatePercentageSplit, addExpense);
+router.get('/expenses/user/:userId', getUserExpenses);
+router.get('/expenses', getOverallExpenses);
 
 module.exports = router;
