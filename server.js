0eaa660c34b38b4062f7852fd95a9dad29b5bfb4
Sonicetorock <sonishgames@gmail.com>
2024-10-23 19:10:34 +0530

    Set up Mongo DB connection

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config();
-
 const app = express();
 
+// middlewares
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
+
+// COnnect to DB
+const { DBConnection } = require('./config/db');
 
 // server ignition
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server ignited on port ${PORT}`);
+    DBConnection();
 });
